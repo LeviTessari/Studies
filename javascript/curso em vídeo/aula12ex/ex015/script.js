669af478307661458e2947cdad50c0fc5194727b
Levi Tessari <levitessari@gmail.com>
2022-06-13 18:23:40 -0300

    up

---
+++ javascript/curso em vídeo/aula12ex/ex015/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    let msg = window.document.getElementById('msg')
+    let paisagem = window.document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora}.`
+
+    if (hora > 0 && hora < 12){
+        //bom dia!
+        paisagem.src = 'manha.jpg'
+    }else if (hora >= 12 && hora < 18){
+        //boa tarde!
+        paisagem.src = 'tarde.jpg'
+
+    }else{
+        //boa noite!
+        paisagem.src = 'noite.jpg'
+    }
+
+}
